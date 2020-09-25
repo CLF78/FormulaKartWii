4d7b113616191019faf368cfbbe3baa999f98d8b
H1dd3nM1nd <twomp021@gmail.com>
2020-09-25 14:22:11 +0200

    Create createbin.py

---
+++ createbin.py
@@ -0,0 +1,15 @@
+import os
+
+
+def main():
+    for file in os.listdir(basepath % 'Riivo'):
+        letter = file[3].lower()
+        with open(os.path.join(basepath % 'Riivo', file), 'rb') as f, open(os.path.join(basepath % 'ISO', 'main%s.bin' % letter), 'wb') as g:
+            bytez = f.read(4)
+            while bytez.hex() != '280016ba':
+                g.write(bytez)
+                bytez = f.read(4)
+        print('Processed', file)
+
+basepath = os.getcwd() + '\\%s\\fkw\\codes'
+main()
