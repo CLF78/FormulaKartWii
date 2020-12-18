fded964360bbbe3645e477b8f65748eb278522de
H1dd3nM1nd <twomp021@gmail.com>
2020-12-18 16:47:02 +0100

    Fixed Title buildscript

--- files/Title/buildtitle.bat
+++ files/Title/buildtitle.bat
@@ -1,13 +1,13 @@
-wimgt encode logo.png --transform=TPL.IA4 --dest=logo.tpl
-copy /b logo.tpl Title_E.d\title\timg\logo.tpl >nul
-copy /b logo.tpl Title_F.d\title\timg\logo.tpl >nul
-copy /b logo.tpl Title_G.d\title\timg\logo.tpl >nul
-copy /b logo.tpl Title_I.d\title\timg\logo.tpl >nul
-copy /b logo.tpl Title_J.d\title\timg\logo.tpl >nul
-copy /b logo.tpl Title_K.d\title\timg\logo.tpl >nul
-copy /b logo.tpl Title_M.d\title\timg\logo.tpl >nul
-copy /b logo.tpl Title_Q.d\title\timg\logo.tpl >nul
-copy /b logo.tpl Title_S.d\title\timg\logo.tpl >nul
-copy /b logo.tpl Title_U.d\title\timg\logo.tpl >nul
+wimgt encode version.png --transform=TPL.IA4 --dest=version.tpl
+copy /b version.tpl Title_E.d\title\timg\version.tpl >nul
+copy /b version.tpl Title_F.d\title\timg\version.tpl >nul
+copy /b version.tpl Title_G.d\title\timg\version.tpl >nul
+copy /b version.tpl Title_I.d\title\timg\version.tpl >nul
+copy /b version.tpl Title_J.d\title\timg\version.tpl >nul
+copy /b version.tpl Title_K.d\title\timg\version.tpl >nul
+copy /b version.tpl Title_M.d\title\timg\version.tpl >nul
+copy /b version.tpl Title_Q.d\title\timg\version.tpl >nul
+copy /b version.tpl Title_S.d\title\timg\version.tpl >nul
+copy /b version.tpl Title_U.d\title\timg\version.tpl >nul
 wszst create *.d
-ren %cd%\logo.tpl
+ren %cd%\version.tpl
