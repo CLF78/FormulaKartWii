78eb16d0463218e4fa75231c714af9c587c2994a
CLF78 <twomp021@gmail.com>
2022-03-09 13:26:06 +0100

    Reorganize assets (build scripts tbd)

--- files/GeoHit/buildgeohit.py
+++
@@ -1,8 +0,0 @@
-import os
-from subprocess import call
-
-geohitlist = ['GeoHitTableItem.txt', 'GeoHitTableItemObj.txt']
-
-# Create GeoHit files
-for geohit in geohitlist:
-    call(['wszst', 'binary', geohit, '--dest=../Common'])
