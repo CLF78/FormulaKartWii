9e37306c83d6cd7ff13176180d61f60225a08552
CLF78 <twomp021@gmail.com>
2022-03-14 20:01:51 +0100

    Add the actual random combo feature

---
+++ src/src/gamemodes/randomcombo.c
@@ -0,0 +1,38 @@
+#include "common.h"
+#include "menudata.h"
+
+char VehicleRestrict;
+u32 CalcRandom();
+u32 GetWeightClass(u32 character);
+
+void RandomComboPickerC(Menudata98* mdata) {
+    // Take splitscreen into account
+    for (int i = 0; i < mdata->localPlayerCount; i++) {
+
+        // Get character excluding Miis
+        u32 character = CalcRandom() % 0x18;
+
+        // Get weight class
+        u32 weightClass = GetWeightClass(character);
+
+        // Use branchless method
+        bool isBike;
+
+        // Get if kart or bike
+        if (VehicleRestrict > 0)
+            isBike = VehicleRestrict - 1;
+        else
+            isBike = CalcRandom() & 1;
+
+        // Get random vehicle
+        u32 vehicle = (CalcRandom() % 6 * 3) + (0x12 * isBike) + weightClass;
+
+        // Store them
+        mdata->charData[i].character = character;
+        mdata->charData[i].vehicle = vehicle;
+
+        // Reset character and vehicle in Racedata to prevent showing the wrong character on course select
+        mdata->prevCharacters[i] = -1;
+        mdata->prevVehicles[i] = -1;
+    }
+}
--- src/src/items/normalrandomizer.c
+++ src/src/items/normalrandomizer.c
@@ -2,81 +2,81 @@
 #include "racedata.h"
 
 typedef struct {
-    unsigned char unk[0x18];
-    unsigned char pid;
+    u8 unk[0x18];
+    u8 pid;
 } ItemHolderPlayer;
 
 typedef struct {
-    unsigned char unk[0x34];
-    unsigned int shockTimer, blueTimer, blooperTimer, powTimer;
+    u8 unk[0x34];
+    u32 shockTimer, blueTimer, blooperTimer, powTimer;
 } ItemSlotData;
 
-extern unsigned char ItemChances[2][19][2];
-extern unsigned char ItemAmounts[2][12];
+extern u8 ItemChances[2][19][2];
+extern u8 ItemAmounts[2][12];
 
-bool NewbieHelper(unsigned int pid);
-unsigned int CalcRandom();
-bool canItemBeGotten(unsigned int item);
+bool NewbieHelper(u32 pid);
+u32 CalcRandom();
+bool canItemBeGotten(u32 item);
 
-unsigned int AmountRandomizer(unsigned int item, unsigned int pid) {
-	bool newbie;
+u32 AmountRandomizer(u32 item, u32 pid) {
+    bool newbie;
 
-	if (pid == 0xC || Racedata->main.scenarios[0].players[pid].playerType != PLAYER_REAL_LOCAL)
-		newbie = false;
-	else
-		newbie = NewbieHelper(pid);
+    if (pid == 0xC || Racedata->main.scenarios[0].players[pid].playerType != PLAYER_REAL_LOCAL)
+        newbie = false;
+    else
+        newbie = NewbieHelper(pid);
 
-    unsigned int rand = CalcRandom() % 100;
-    unsigned int amount = 1;
-	unsigned int doubleChance = ItemChances[newbie][item][0];
-	unsigned int tripleChance = ItemChances[newbie][item][1];
+    u32 rand = CalcRandom() % 100;
+    u32 amount = 1;
+    u32 doubleChance = ItemChances[newbie][item][0];
+    u32 tripleChance = ItemChances[newbie][item][1];
 
-	if (rand >= doubleChance) {
-		amount++;
-		if (rand >= tripleChance)
-			amount++;
-	}
+    if (rand >= doubleChance) {
+        amount++;
+        if (rand >= tripleChance)
+            amount++;
+    }
 
-	if (pid != 0xC) {
-		ItemAmounts[0][pid] = ItemAmounts[1][pid];
-		ItemAmounts[1][pid] = amount;
-	}
+    if (pid != 0xC) {
+        ItemAmounts[0][pid] = ItemAmounts[1][pid];
+        ItemAmounts[1][pid] = amount;
+    }
 
-	return amount;
+    return amount;
 }
 
-unsigned int UltimateRandom(ItemSlotData *slot, unsigned int itemBoxSetting, unsigned int position, bool isHuman, bool unused, ItemHolderPlayer* player) {
-	bool newbie = NewbieHelper(player->pid);
-	unsigned int modulo = 20;
-    unsigned int increment = 0;
-	if (newbie && isHuman) {
-		modulo = 10;
+u32 UltimateRandom(ItemSlotData *slot, u32 itemBoxSetting, u32 position, bool isHuman, bool unused, ItemHolderPlayer* player) {
+    bool newbie = NewbieHelper(player->pid);
+    u32 modulo = 20;
+    u32 increment = 0;
+    if (newbie && isHuman) {
+        modulo = 10;
         increment = 7;
     }
 
-	while (true) {
-        unsigned int item = ((CalcRandom() % modulo) % 16) + increment;
+    while (true) {
+        u32 item = ((CalcRandom() % modulo) % 16) + increment;
         if (item == 0x5)
             continue;
-		else if (item == 0xC) {
-			if (!isHuman || newbie)
-				continue;
-		}
-		else if (item == 0x10)
-		    item = 4;
+        else if (item == 0xC) {
+            if (!isHuman || newbie)
+                continue;
+        }
+        else if (item == 0x10)
+            item = 4;
 
-		else if (item == 0x7) {
+        else if (item == 0x7) {
             if (slot->blueTimer || (!isHuman && position == 1))
                 continue;
         }
-		else if (item == 0x8) {
+        else if (item == 0x8) {
             if (slot->shockTimer || !canItemBeGotten(item))
-			    continue;
+                continue;
         }
-		else if (item == 0xD) {
+        else if (item == 0xD) {
             if (slot->powTimer || !canItemBeGotten(item) || (!isHuman && position == 1))
                 continue;
         }
-		return item;
+        return item;
     }
 }
