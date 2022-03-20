4e14fc6b679cfca021fc444c760186b3b9982aa1
CLF78 <twomp021@gmail.com>
2022-03-20 23:09:23 +0100

    Fix AltKMP memory allocation fail

--- src/src/fixes/trackident.c
+++ src/src/fixes/trackident.c
@@ -4,42 +4,47 @@
 #include "stdlib.h"
 
 extern KMPHeader* AltKMP;
+u32 FindContainHeap(void* buffer); // incorrect signature, whatever
 
-void TrackIdentifierLoad(u32 crc) {
-	char buffer[64];
-	DVDHandle handle;
+void TrackIdentifierLoad(u32 crc, void* ogSection) {
+    char buffer[64];
+    DVDHandle handle;
 
-	// Print filename in buffer
-	snprintf(buffer, 64, "/Race/Course/Patches/%08X.kmp", crc);
+    // Print filename in buffer
+    snprintf(buffer, 64, "/Race/Course/Patches/%08X.kmp", crc);
 
-	// Open file
-	bool fileOpened = DVDOpen(buffer, &handle);
+    // Open file
+    bool fileOpened = DVDOpen(buffer, &handle);
 
-	// Check if file exists
-	if (fileOpened) {
-		// If so, get its length and allocate it
-		void* kmpData = new(handle.length, 32);
+    // Check if file exists
+    if (fileOpened) {
+        // If so, get its length and allocate it
+        void* kmpData = new(handle.length, (void*)(FindContainHeap(ogSection) - 0x38), 32);
 
-		// Store static instance
-		AltKMP = (KMPHeader*)kmpData;
+        // If allocation failed, bail
+        if (kmpData != 0) {
 
-		// Read the file there
-		DVDReadPrio(&handle, kmpData, handle.length, 0, 2);
+            // Store static instance
+            AltKMP = (KMPHeader*)kmpData;
 
-		// Close the file
-		DVDClose(&handle);
-	}
+            // Read the file there
+            DVDReadPrio(&handle, kmpData, handle.length, 0, 2);
+        }
+
+        // Close the file
+        DVDClose(&handle);
+    }
 }
 
 u32* GetAltKMPSection(u32* originalSection, u32 magic) {
-	if (AltKMP == 0)
-		return originalSection;
+    if (AltKMP == 0)
+        return originalSection;
 
-	for (int i = 0; i < AltKMP->numSections; i++) {
-		u32* currSection = (u32*)((u32)AltKMP + AltKMP->headerLength + AltKMP->sectionOffsets[i]);
-		if (*currSection == magic)
-			return currSection;
-	}
+    for (int i = 0; i < AltKMP->numSections; i++) {
+        u32* currSection = (u32*)((u32)AltKMP + AltKMP->headerLength + AltKMP->sectionOffsets[i]);
+        if (*currSection == magic)
+            return currSection;
+    }
 
-	return originalSection;
+    return originalSection;
 }
