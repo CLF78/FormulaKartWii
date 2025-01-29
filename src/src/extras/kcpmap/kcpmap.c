692d06440308175cdac91e7b506896392e226035
Conni243 <79007910+Conni243@users.noreply.github.com>
2025-01-29 20:07:13 +0100

    Initial Scaling Implementation
    Need to make math.h for my own sanity after this
    Also calculate the correct scaling factor on the fly (also overkill sqrt)

--- src/src/extras/kcpmap/kcpmap.c
+++ src/src/extras/kcpmap/kcpmap.c
@@ -3,13 +3,41 @@
 #include "stdlib.h"
 
 typedef struct{
+    u8 unk[0x28];
+    Vec3 translation;
+    Vec3 rotation;
+    Vec2 scale;
+    float width;
+    float height;
+    u8 unk1[0x64];
+    u8 alpha;
+    u8 field23_0xb5;
+    u8 originType;
+    u8 flags;
+    char name[17]; // Created by retype action
+    char userInfo[9];
+    u8 unk3[0x2];
+} Pane_base;
+
+typedef struct{
     void* vtable;
-    u8 unk[0x198];
-    f32 x;
-    u8 unk1[0x4];
-    f32 y;
-    u8 unk2[0x4];
-    f32 rotation;
+    Pane_base base;
+} Pane;
+
+typedef struct {
+    Vec3 tranlate;
+    Vec2 scale;
+    u8 opacity;
+    u8 padding[0x3];
+} Element;
+
+typedef struct{
+    void* vtable;
+    Element elements[4];
+    u8 unk[0x134];
+    Pane *pane;
+    Vec3 position;
+    Vec3 rotation;
 } MapObject;
 
 typedef struct{
@@ -21,6 +49,7 @@ extern void* CtrlRace2dMapObject_vt;
 extern void* LayoutUIControl_construct(void* control);
 extern void UIControl_insertChild(void* control, u32 index, void* child);
 extern void ControlLoader_load(CtrlLoader* ControlLoader, char* dirname, char* filename, char* variantname, char** animations);
+extern void CtrlRace2dMapObject_initSelf(MapObject *object);
 extern f32 mkw_atan2(f32 x, f32 y);
 extern char s_game_image; 
 extern char s_map_start_line;
@@ -34,6 +63,14 @@ u32 GetNKCPs() {
         }
     }
     return count;
+}
+
+f32 sqrt_ap(f32 a){
+    f32 ig = a;
+    for (int i = 0; i < 10; i++) {
+        ig = 0.5 * (ig + a / ig);
+    }
+    return ig; 
 }
 
 u32 InsertKCPs(void* MapCtrl, u32 prevChildIndex, CtrlLoader* ctrlLoader){
@@ -51,10 +88,15 @@ u32 InsertKCPs(void* MapCtrl, u32 prevChildIndex, CtrlLoader* ctrlLoader){
             ctrlLoader->destination = kcp;
             ControlLoader_load(ctrlLoader, &s_game_image, &s_map_start_line, &s_start_line, 0); 
 
-            kcp->x = checkpoint->mMidpoint.x;
-            kcp->y = checkpoint->mMidpoint.y;
+            kcp->position.x = checkpoint->mMidpoint.x; 
+            kcp->position.z = checkpoint->mMidpoint.y; //accept the way of life
 
-            kcp->rotation = mkw_atan2(checkpoint->mDir.x, checkpoint->mDir.y) * 57.2958;
+            kcp->rotation.y = mkw_atan2(checkpoint->mDir.x, checkpoint->mDir.y) * 57.2958;
+
+            CtrlRace2dMapObject_initSelf(kcp); //normally it does this automatically later but I have to do it now if I want to modify the scale
+
+            kcp->pane->base.width = fsqrt((checkpoint->mpData->p0.x - checkpoint->mpData->p1.x)*(checkpoint->mpData->p0.x - checkpoint->mpData->p1.x) + (checkpoint->mpData->p0.y - checkpoint->mpData->p1.y)*(checkpoint->mpData->p0.y - checkpoint->mpData->p1.y)) / 4000;
+
         }
     }
     return prevChildIndex;
