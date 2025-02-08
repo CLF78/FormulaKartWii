2815eaefa219ca43467ae3d45517a77a9e0078df
Conni243 <79007910+Conni243@users.noreply.github.com>
2025-02-08 22:38:08 +0100

    Use custom asset instead
    updated string references to point to a custom brctr and added placeholder assets

--- src/include/math.h
+++ src/include/math.h
@@ -10,7 +10,7 @@ extern f64 atan(f64 x);
 extern f64 atan2(f64 x, f64 y); //this implementation is broken? it doesn't work 50% of the time
 extern f64 acos(f64 x);
 
-extern f64 rd_atan2(f64 x, f64 y);
+extern f64 rd_atan2(f64 x, f64 y); //using this radian based atan2 I found (It works)
 
 extern f64 pow(f64 x, f64 y);
 extern f64 sqrt(f64 x);
