2f015ff45351948f5c3981c0443db795fad024c3
TheIsmy64 <cairoliijpm4@gmail.com>
2025-06-04 17:36:00 +0200

    γ1.8.3 release
    * Fixed the My Stuff not working when FCIs are turned on.
    * Bumped up internal version for the release.

--- src/src/main.c
+++ src/src/main.c
@@ -270,7 +270,7 @@ void loadCodes() {
 	directWriteBranch(GuestSendHook, GuestSend, false);
 	directWriteBranch(HostCheckHook, HostCheck, false);
 	directWriteBranch(HostCheckHelperHook, HostCheckHelper, true);
-	directWrite8(Version, 13);
+	directWrite8(Version, 14);
 
 	// Impervious TC (by CLF78)
 	directWrite32(ImperviousTCHook, 0x48000038);
