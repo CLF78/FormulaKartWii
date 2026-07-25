2847b335575ccb670413549d12a1ffb150465bfe
TheIsmy64 <cairoliijpm4@gmail.com>
2026-07-25 13:36:15 +0200

    γ1.9.3-β.1
    * Fixed a bad bullet bill drop-off on KC.
    * Version update.

--- src/src/main.c
+++ src/src/main.c
@@ -303,7 +303,7 @@ void loadCodes() {
 	directWriteBranch(GuestSendHook, GuestSend, false);
 	directWriteBranch(HostCheckHook, HostCheck, false);
 	directWriteBranch(HostCheckHelperHook, HostCheckHelper, true);
-	directWrite8(Version, 192);
+	directWrite8(Version, 101);
 
 	// Impervious TC (by CLF78)
 	directWrite32(ImperviousTCHook, 0x48000038);
